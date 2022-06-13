2c58e4b2f58d4db8f878a2cb88a9cee69e28fd10
rounak <rounakshaw903@gmail.com>
2022-06-13 13:55:35 +0530

    initial commit

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,25 +1,42 @@
 import "./testimonials.scss";
 
-export default function testimonials() {
+export default function Testimonials() {
+  const data = [
+    {
+      id: 1,
+      name: "Rounak Kr. Shaw",
+      title: "Passionate Coder",
+      img:
+        "assets/rounak.png",
+      icon: "assets/linkedin.png",
+      desc:
+        "Currently pursuing my Btech from IIT BHU(Varanasi), passionate about coding and Web Development. Listening msic is my hobby.",
+    },
+  ];
   return (
     <div className="testimonials" id="testimonials">
       <h1>Testimonials</h1>
       <div className="container">
-        <div className="card">
-          <div className="top">
-            <img src="assets/right-arrow.png" className="left" alt="" />
-            <img className="user" src="assets/rounak.png" alt="" />
-            <img class="right" src="assets/youtube.png" alt="" />
+        {data.map((d) => (
+          <div className={d.featured ? "card featured" : "card"}>
+            <div className="top">
+              <img src="assets/right-arrow.png" className="left" alt="" />
+              <img
+                className="user"
+                src={d.img}
+                alt=""
+              />
+              <img className="right" src={d.icon} alt="" />
+            </div>
+            <div className="center">
+              {d.desc}
+            </div>
+            <div className="bottom">
+              <h3>{d.name}</h3>
+              <h4>{d.title}</h4>
+            </div>
           </div>
-          <div className="center">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem
-            voluptas dolorum dolores esse in, alias sit
-          </div>
-          <div className="bottom">
-            <h3>Rounak</h3>
-            <h4>My Portfolio</h4>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,19 +1,27 @@
 import "./topbar.scss"
-import {Person,Mail} from "@mui/icons-material"
+import {Person,Mail, LinkedIn, GitHub} from "@mui/icons-material"
 
 export default function topbar({menuOpen, setMenuOpen}) {
   return (
     <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo">genius.</a>
+          <a href="#intro" className="logo">My Portfolio</a>
           <div className="itemContainer">
               <Person className="icon"/>
               <span>+91 8697380479</span>
           </div>
           <div className="itemContainer">
               <Mail className="icon"/>
-              <span>rounakshaw903@gamil.com</span>
+              <span>rounak.krshaw.met20@itbhu.ac.in</span>
+          </div>
+          <div className="itemContainer">
+              <LinkedIn className="icon"/>
+              <span>linkedin.com/in/rounak-kr-shaw-907ba3227</span>
+          </div>
+          <div className="itemContainer">
+              <GitHub className="icon"/>
+              <span>github.com/Rounak-2002</span>
           </div>
         </div>
         <div className="right">
